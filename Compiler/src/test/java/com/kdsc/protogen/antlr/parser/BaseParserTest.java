96d3e9a59d613464167a669df0c569e986cb15d7
Keith <KeithMDavies@hotmail.com>
2021-09-14 12:42:44 +0100

    Fixed BaseParserTest

--- Compiler/src/test/java/com/kdsc/protogen/antlr/parser/BaseParserTest.java
+++ Compiler/src/test/java/com/kdsc/protogen/antlr/parser/BaseParserTest.java
@@ -6,7 +6,9 @@ import com.kdsc.protogen.antlr.ProtoGenVisitorImplementation;
 import com.kdsc.protogen.antlr.errors.ProtoGenErrorListener;
 import com.kdsc.protogen.parsetree.FileNode;
 import org.antlr.v4.runtime.ANTLRInputStream;
+import org.antlr.v4.runtime.BaseErrorListener;
 import org.antlr.v4.runtime.CommonTokenStream;
+import org.antlr.v4.runtime.tree.ParseTree;
 
 import java.io.ByteArrayInputStream;
 import java.io.IOException;
@@ -22,65 +24,62 @@ public abstract class BaseParserTest {
         System.out.println("//Test Program");
         System.out.println(testProgram);
 
-        var inputStream = new ByteArrayInputStream(testProgram.getBytes(StandardCharsets.UTF_8));
+        var errorListener = new ProtoGenErrorListener();
 
-        try {
-            var antlrInputStream = new ANTLRInputStream(inputStream);
-            var lexer = new ProtoGenLexer(antlrInputStream);
-            var tokens = new CommonTokenStream(lexer);
-            var parser = new ProtoGenParser(tokens);
-            parser.removeErrorListeners();
-            var errorListener = new ProtoGenErrorListener();
-            parser.addErrorListener(errorListener);
-            var visitor = new ProtoGenVisitorImplementation();
-            var node = (FileNode) visitor.visit(parser.file());
+        var parseTree = compileTestProgramAndReturnParseTree(errorListener, testProgram);
 
-            if(errorListener.errorOccurred()) {
-                for(var message : errorListener.getErrors()) {
-                    System.out.println(message);
-                }
-                fail("Expected test program to compile without error");
+        var visitor = new ProtoGenVisitorImplementation();
+        var node = (FileNode) visitor.visit(parseTree);
+
+        if(errorListener.errorOccurred()) {
+            for(var message : errorListener.getErrors()) {
+                System.out.println(message);
             }
-
-            System.out.println("//Parse Tree");
-            System.out.println(node.toFormattedString(1));
-
-        } catch (IOException e) {
-            e.printStackTrace();
+            fail("Expected test program to compile without error");
         }
+
+        System.out.println("//Parse Tree");
+        System.out.println(node.toFormattedString(1));
+
     }
 
     protected List<String> compileTestProgramReturnParserErrors(String testProgram) {
+
         System.out.println("//Test Program");
         System.out.println(testProgram);
 
-        var inputStream = new ByteArrayInputStream(testProgram.getBytes(StandardCharsets.UTF_8));
-
         var errorListener = new ProtoGenErrorListener();
 
-        try {
-            var antlrInputStream = new ANTLRInputStream(inputStream);
-            var lexer = new ProtoGenLexer(antlrInputStream);
-            var tokens = new CommonTokenStream(lexer);
-            var parser = new ProtoGenParser(tokens);
-            parser.removeErrorListeners();
-            parser.addErrorListener(errorListener);
-            var visitor = new ProtoGenVisitorImplementation();
-            visitor.visit(parser.file());
+        compileTestProgramAndReturnParseTree(errorListener, testProgram);
 
-            System.out.println("//Error Messages");
-            if(!errorListener.errorOccurred()) {
-                System.out.println("None".indent(4));
-            }
-
-            for(var message : errorListener.getErrors()) {
-                System.out.println(message.indent(4));
-            }
-
-        } catch (IOException e) {
-            e.printStackTrace();
+        System.out.println("//Error Messages");
+        if(!errorListener.errorOccurred()) {
+            System.out.println("None".indent(4));
+        }
+        for(var message : errorListener.getErrors()) {
+            System.out.println(message.indent(4));
         }
 
         return errorListener.getErrors();
+    }
+
+    private ParseTree compileTestProgramAndReturnParseTree(BaseErrorListener errorListener, String testProgram) {
+
+        var inputStream = new ByteArrayInputStream(testProgram.getBytes(StandardCharsets.UTF_8));
+
+        ANTLRInputStream antlrInputStream;
+        try {
+            antlrInputStream = new ANTLRInputStream(inputStream);
+        } catch (IOException exception) {
+            throw new RuntimeException(exception);
+        }
+
+        var lexer = new ProtoGenLexer(antlrInputStream);
+        var tokens = new CommonTokenStream(lexer);
+        var parser = new ProtoGenParser(tokens);
+
+        parser.removeErrorListeners();
+        parser.addErrorListener(errorListener);
+        return parser.file();
     }
 }
