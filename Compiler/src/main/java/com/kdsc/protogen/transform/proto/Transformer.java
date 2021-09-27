17fcdc76aa2e48ea3a6b551e7ee99bd9c5a6b773
Keith <KeithMDavies@hotmail.com>
2021-09-27 14:38:03 +0100

    Upgrade to Java 17 with enable preview on, only upgrade the toolchain in Gradle not the Gradle runtime.  Starting to get Groovy warnings

--- Compiler/src/main/java/com/kdsc/protogen/transform/proto/Transformer.java
+++ Compiler/src/main/java/com/kdsc/protogen/transform/proto/Transformer.java
@@ -2,15 +2,26 @@ package com.kdsc.protogen.transform.proto;
 
 import com.kdsc.protogen.filegenerationtree.FileNode;
 
+import com.kdsc.protogen.parsetree.ProtoGenEnumNode;
 import com.kdsc.protogen.transform.TransformerContext;
 
 import java.util.Collections;
 import java.util.List;
 
+
 public class Transformer implements com.kdsc.protogen.transform.Transformer {
 
     @Override
     public List<FileNode> transform(TransformerContext transformerContext, List<com.kdsc.protogen.parsetree.FileNode> fileNodes) {
+        fileNodes
+            .stream().flatMap(fn -> fn.getProtoGenEnumNodes().stream())
+            .forEach(
+                fn -> {
+                    switch (fn) {
+                        case ProtoGenEnumNode protoGenEnumNode -> System.out.println("EnumNode");
+                    }
+                }
+            );
         return Collections.emptyList();
     }
 }
