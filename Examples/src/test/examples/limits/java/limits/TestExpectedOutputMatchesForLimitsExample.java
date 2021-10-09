7649e6ddfd8f5230605dd45715991f3d713fa192
Keith <KeithMDavies@hotmail.com>
2021-10-09 02:58:06 +0100

    Tried to find the limit to nested sets

--- Examples/src/test/examples/limits/java/limits/TestExpectedOutputMatchesForLimitsExample.java
+++ Examples/src/test/examples/limits/java/limits/TestExpectedOutputMatchesForLimitsExample.java
@@ -1,10 +1,12 @@
 package limits;
 
 import com.kdsc.protogen.examples.BaseExpectedOutputMatchesTest;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
 public final class TestExpectedOutputMatchesForLimitsExample extends BaseExpectedOutputMatchesTest {
 
+    @Disabled //Will be too slow to run all the time
     @Test
     public void testExpectedOutputMatches() {
         runCompilerWithTestProgram("limits", "", "limits");
--- Examples/src/test/examples/limits/java/limits/TestSerialisationForLimitsExample.java
+++ Examples/src/test/examples/limits/java/limits/TestSerialisationForLimitsExample.java
@@ -1,9 +1,11 @@
 package limits;
 
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
 public final class TestSerialisationForLimitsExample {
 
+    @Disabled //Will be too slow to run all the time
     @Test
     public void testSerialisation() {
 
